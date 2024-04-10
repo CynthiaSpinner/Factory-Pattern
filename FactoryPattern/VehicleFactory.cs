e79eab5b25de3b49ba4a7a2e68a70707a51610b5
Cynthia <CynthiaSpinner@gmail.com>
2024-04-10 14:13:27 -0500

    finished exercise 1

---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int tires)
+        {
+            switch (tires)
+            {
+                case 4:
+                    return new Car();
+                case 2:
+                    return new Motorcycle();
+                default:
+                    return new Car();
+            }
+
+        }
+    }
+}
